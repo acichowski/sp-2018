e4b84e88088776ca72c579e20e0c239cda2de2e1
acichowski <44109713+acichowski@users.noreply.github.com>
2018-12-14 08:40:43 +0100

    Create zad1.c

---
+++ Elementarz 1/zad1.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void wyswietl();
+
+
+int main()
+{
+  wyswietl();
+}
+void wyswietl()
+{
+  int a;
+  printf("n1=%d\n",5+3*5/2-3);
+  printf("n2=%d\n",2%2+2*2-2/2);
+  printf("n3=%d\n",2*4*(5+9/2));
+  exit(1);
+}
